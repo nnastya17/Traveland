a8daa61f7daa53014f22d67dbe0b3fa5cca0b818
nnastya17 <nnastya1703@gmail.com>
2021-11-23 11:46:35 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:3,
+		slidesToScroll:2,
+		speed:2000,
+		autoplay:true,
+		autoplaySpeed:2000
+	});
+});
